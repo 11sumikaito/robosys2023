e11b00bbf174efc99d345c286cc79a94be8808f6
Sumi Kaito <s22C1074BP@s.chibakoudai.jp>
2023-11-02 11:31:49 +0900

    update

--- test.bash
+++ test.bash
@@ -1,3 +1,18 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Kaito Sumi s22C1074BP@s.chibakoudai.jp
+# SPDX-License-Identifier: BSD-3-Clause
+ng (){
+	echo NG at Line $1
+	res=1
+}
 
-seq 5 | ./plus
+res=0
+
+
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
+

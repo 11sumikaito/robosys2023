1ba84ef40699d6ea9b12f2aae32909b81189f8ac
Sumi Kaito <s22C1074BP@s.chibakoudai.jp>
2023-11-02 12:23:14 +0900

    update

--- test.bash
+++ test.bash
@@ -2,23 +2,23 @@
 # SPDX-FileCopyrightText: 2023 Kaito Sumi s22C1074BP@s.chibakoudai.jp
 # SPDX-License-Identifier: BSD-3-Clause
 
-$ ./test.bash
+ng(){
+	echo NG at Line $1
+	res=1
+}
 
 res=0
-+ res=0
-
 
 out=$(seq 5 | ./plus)
-++ seq 5
-++ ./plus
-+ out=15
-[ "${out}" = 15] || ng ${LINENO}
-+ '[' 15 = 15 ']'
+[ "${out}" = 15]  || ng ${LINENO}
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
-+ '[' 0 = 0 ']'
-+ echo OK
-OK
 exit $res
-+ exit 0
-

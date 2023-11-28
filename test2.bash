f2e1cc3d1d3873b2734508d432ad6a40dd76c6a3
Sumi Kaito <s22C1074BP@s.chibakoudai.jp>
2023-11-28 13:25:37 +0900

    update

--- test2.bash
+++ test2.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash
 # SPDX-FileCopyrightText: 2023 11sumikaito
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -10,11 +10,11 @@ ng() {
 res=0
 
 # 1990年のワールドカップ優勝国が正しく表示されるかを確認する
-out=$(echo 1990 | ./world_cup_winner.py)
+out=$(./world_cup_winner 1990)
 [ "${out}" = "1990年のワールドカップ優勝国は 西ドイツ（現在のドイツ） です。" ] || ng ${LINENO}
 
 # 存在しない年数を指定したときに該当する年代のデータが見つからないメッセージが表示されるかを確認する
-out=$(echo 1800 | ./world_cup_winner.py)
+out=$(./world_cup_winner 1800)
 [ "${out}" = "1800年の該当する年代のデータが見つかりませんでした。" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
